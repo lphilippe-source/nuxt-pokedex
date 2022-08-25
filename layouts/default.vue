11536afc70c68751fdc836f71dd32b492b6456cb
Philippe Lemaire <philippelmr@gmail.com>
2022-08-25 19:38:56 +0200

    feat: store: store and layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+   <HeaderComponent/>
+    <NavigationMenuComponent/>
+  <nuxt/>
+<FooterComponent/>
+  </div>
+</template>
+<script>
+import HeaderComponent from '../components/header.vue'
+import FooterComponent from '../components/footer.vue'
+import NavigationMenuComponent from "@/components/navigationMenu";
+export default{
+  name:"DefaultLayout",
+  components:{
+    HeaderComponent,
+    FooterComponent,
+    NavigationMenuComponent
+  },
+  }
+</script>
